fb7ce6337a4358dbde7e8c21cc63a70e77b100e0
anurajbh <anurajbh@users.noreply.github.com>
2020-03-06 14:56:14 -0500

    Update DialogueTrigger.cs
    TO-DO cause buttons to have effects on Player Stats

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
+    Button button;
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().startDialogue(dialogue);
+    }
+    public void TriggerStats()
+    {
+        switch(button.name)
+        {
+            default:
+            break;
+                //TODO- Check for button names and cause stuff to happen to Player Anxiety and Will depending on buttonname 
+        }
     }
 }
