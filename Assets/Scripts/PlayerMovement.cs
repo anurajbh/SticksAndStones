4d4a682aff5bd27dd5ebd50abcbe4b1ad6f08e65
anurajbh <anurajbh@users.noreply.github.com>
2020-03-21 17:27:58 -0400

    Tile-based Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject PlayerBody;
-    public float walkSpeed =5f;
-    public float sprintSpeed = 5f;// for future sprint implementation
+   
+    public float walkSpeed =1f;
+    public bool hasMoved = false;
+    private Vector2 movementInput;
+
+    private Vector3 movementdirection;
     private void Awake()
     {
         
@@ -16,18 +19,98 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         GetPlayerInput();
+        if(movementInput.x == 0 && movementInput.y == 0)
+        {
+            hasMoved = false;
+        }
+        else if ((movementInput.x !=0 && !hasMoved) || (movementInput.y != 0 && !hasMoved) )
+        {
+            hasMoved = true;
+            GetPlayerDirection();
+        }
+        
     }
 
-    private void GetPlayerInput()//TO-DO- Sprint Implementation
+    private void GetPlayerInput()
     {
-        float xPos = Input.GetAxisRaw("Horizontal");//to ensure that movement input is discrete
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void GetPlayerDirection()
+    {
+        if (movementInput.x < 0)
+        {
+            if (movementInput.y < 0)
+            {
+                movementdirection = new Vector3(-walkSpeed / 2f, -walkSpeed / 2f);
+            }
+            else if (movementInput.y > 0)
+            {
+                movementdirection = new Vector3(-walkSpeed / 2f, walkSpeed / 2f);
+            }
+            else
+            {
+                movementdirection = new Vector3(-walkSpeed, 0f);
+            }
+            transform.position += movementdirection;
+        }
+        if (movementInput.x > 0)
+        {
+            if (movementInput.y < 0)
+            {
+                movementdirection = new Vector3(walkSpeed / 2f, -walkSpeed / 2f);
+            }
+            else if (movementInput.y > 0)
+            {
+                movementdirection = new Vector3(walkSpeed / 2f, walkSpeed / 2f);
+            }
+            else
+            {
+                movementdirection = new Vector3(walkSpeed, 0f);
+            }
+            transform.position += movementdirection;
+        }
+        if (movementInput.y > 0)
+        {
+            if (movementInput.x < 0)
+            {
+                movementdirection = new Vector3(-walkSpeed / 2f, walkSpeed / 2f);
+            }
+            else if (movementInput.x > 0)
+            {
+                movementdirection = new Vector3(walkSpeed / 2f, walkSpeed / 2f);
+            }
+            else
+            {
+                movementdirection = new Vector3(0f, walkSpeed);
+            }
+            transform.position += movementdirection;
+        }
+        if (movementInput.y < 0)
+        {
+            if (movementInput.x < 0)
+            {
+                movementdirection = new Vector3(-walkSpeed / 2f, -walkSpeed / 2f);
+            }
+            else if (movementInput.x > 0)
+            {
+                movementdirection = new Vector3(walkSpeed / 2f, -walkSpeed / 2f);
+            }
+            else
+            {
+                movementdirection = new Vector3(0f, -walkSpeed);
+            }
+            transform.position += movementdirection;
+        }
+        /*float xPos = Input.GetAxisRaw("Horizontal");//to ensure that movement input is discrete
         float yPos = Input.GetAxisRaw("Vertical");//to ensure that movement input is discrete
         float xMovement = xPos * walkSpeed * Time.deltaTime;
         float yMovement = yPos * walkSpeed * Time.deltaTime;
 
         float xMove = transform.localPosition.x + xMovement;
         float yMove = transform.localPosition.y + yMovement;
-        transform.localPosition = new Vector3(xMove, yMove, -0.1f);
+        transform.localPosition = new Vector3(xMove, yMove, -0.1f);*/
     }
-
 }
+
