4cc74c4213d843234169c369de1dfa50b54ea75e
Lilly Zhu <yzhu608@gatech.edu>
2020-03-22 21:00:15 -0400

    will bar
    will bar testing

---
+++ Assets/WillBarScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WillBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+    public void SetMaxWill(int will)
+    {
+    	slider.maxValue = will;
+    	slider.value = will;
+    }
+    public void setWill(int will)
+    {
+    	slider.value = will; 
+    }
+}
