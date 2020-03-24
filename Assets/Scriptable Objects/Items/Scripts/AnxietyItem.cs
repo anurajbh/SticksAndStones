37eb5a78e82513dfedb7301224d72ca63ecf7be5
Luke Austin Passmore <lpassmore3@gatech.edu>
2020-03-24 11:22:23 -0400

    Created Will and Anxiety items

---
+++ Assets/Scriptable Objects/Items/Scripts/AnxietyItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Anxiety Item", menuName = "Inventory System/Items/Anxiety")]
+
+public class AnxietyItem : ItemObject
+{
+    public float anxietyChange = 10f;
+    public void Awake() {
+        type = ItemType.Will;
+    }
+}
---
+++ Assets/Scriptable Objects/Items/Scripts/WillItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Will Item", menuName = "Inventory System/Items/Will")]
+
+public class WillItem : ItemObject
+{
+    public float willChange = 10f;
+    public void Awake() {
+        type = ItemType.Will;
+    }
+}
