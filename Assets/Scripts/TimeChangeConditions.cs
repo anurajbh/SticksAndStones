e0985f94b15a7f34a6bf4eabafb4e3253f02027d
anurajbh <anurajbh@users.noreply.github.com>
2020-03-29 14:30:45 -0400

    Time changing and Day Night cycle functionalities implemented- TODO- actual implementation of the cycle in scene building

---
+++ Assets/Scripts/TimeChangeConditions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeChangeConditions : MonoBehaviour
+{
+    TimeProgression timeProgression;
+    public List<bool> spokenTo;
+    public int ctr = 0;
+    public int max = 0;
+    private void Update()
+    {
+        CheckForConditions();
+    }
+
+    private void CheckForConditions()
+    {
+        for(int i =0; i <= spokenTo.Count; i++)
+        {
+            if (spokenTo[i])
+            {
+                max++;
+            }
+
+        }
+        for(int i = 0; i <= max; i++)
+        {
+            if (ctr == 0)
+            {
+                timeProgression.myCycle = TimeProgression.cycle.dawn;
+            }
+            else if (ctr == 1)
+            {
+                timeProgression.myCycle = TimeProgression.cycle.noon;
+            }
+            else if (ctr == 2)
+            {
+                timeProgression.myCycle = TimeProgression.cycle.dusk;
+            }
+            else if (ctr == 3)
+            {
+                timeProgression.myCycle = TimeProgression.cycle.night;
+            }
+        }
+       
+    }
+}
