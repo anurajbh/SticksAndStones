a09c8b494396d3c550d9b0aad628438b9326e56e
anurajbh <anurajbh@users.noreply.github.com>
2020-03-31 14:47:09 -0400

    Prefabs of PlayerCharacter, Camera, Menu/Inventory. Also added modifications to WIllBar script to make it a StatsBar script
    StatsBar keeps track of two sliders and two different values(Will and Anxiety).
    
    Prefabs for PC, Camera, and Menu/Inventory in folder.

---
+++ Assets/Scripts/StatsBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsBar : MonoBehaviour
+{
+    public Slider WillSlider;
+    public Slider AnxietySlider;
+    public PlayerStats playerStats;
+    private void Awake()
+    {
+        playerStats = GameObject.Find("PlayerController").GetComponent<PlayerStats>();
+        SetWillBar(playerStats.will);
+        SetAnxietyBar(playerStats.anxiety);
+    }
+    private void Update()
+    {
+        SetWill(playerStats.will);
+        SetAnxiety(playerStats.anxiety);
+    }
+
+    public void SetWillBar(float will)
+    {
+    	WillSlider.maxValue = will;
+    }
+
+    public void SetWill(float will)
+    {
+    	WillSlider.value = will;
+    }
+
+    public void SetAnxietyBar(float anxiety)
+    {
+        AnxietySlider.maxValue = anxiety;
+    }
+
+    public void SetAnxiety(float anxiety)
+    {
+        AnxietySlider.value = anxiety;
+    }
+}
