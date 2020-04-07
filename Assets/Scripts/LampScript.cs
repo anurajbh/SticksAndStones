bedb9be5bde70407db99bba01c55081c23a2b8d0
Amrithesh Paravath <aparavath3@gatech.edu>
2020-04-06 22:50:43 -0300

    Implemented lighting into DayNight Now DayNight has 4 phases which change the directional light and any point lights in the scene. Added a test scene to see how it works. Edit the script's awake function to see the different phases. Settings are just what I thought works, but can change. Added Elly spritesheet too.

---
+++ Assets/Scripts/LampScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampScript : MonoBehaviour
+{
+    public bool isLamp;
+    public void changeLampIntesity(float intensity) {
+        if (isLamp) {
+            GetComponent<Light>().intensity = intensity;
+        }
+    }
+}
