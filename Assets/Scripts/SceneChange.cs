0418bb2a32988cd3259184b6cdfe293bf1ece364
nbayrouti3 <noura@beirutis.com>
2020-04-10 15:30:40 -0700

    Added Main Menu, Hooked up scene changes and set up a really rough build

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene("OutsideTest");
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            SceneManager.LoadScene("Floor2");
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene("Floor1");
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            SceneManager.LoadScene("OutsideTest");
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
