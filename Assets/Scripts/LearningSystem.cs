cff7a387e403ffbf410e6046680a631eb3ae436f
Amrithesh Paravath <aparavath3@gatech.edu>
2020-05-24 14:45:16 -0300

    Learning System v2, started implementation with new player stats

--- Assets/Scripts/LearningSystem.cs
+++ Assets/Scripts/LearningSystem.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class LearningSystem : MonoBehaviour
 {
-    Skills skills;
     SMPlayerStats player;
+    public Skills ;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<SMPlayerStats>();
+        skills = GetComponent<Skills>();
     }
 
     //Adds a skill onto skills
     private void learnSkill(Skill skill) {
         //Todo: get skills from the entity
-        skills.Learn(skill.skillName, skill.anxiety, skill.will, skill.enemyDamage);
+        player = GameObject.FindWithTag("Player").GetComponent<SMPlayerStats>();
     } 
 }
