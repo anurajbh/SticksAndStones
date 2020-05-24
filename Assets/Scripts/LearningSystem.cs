1ff436d5089b27e9e977454d4d01ac406744b931
Amrithesh Paravath <aparavath3@gatech.edu>
2020-05-23 21:35:56 -0300

    Learning system v1

---
+++ Assets/Scripts/LearningSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningSystem : MonoBehaviour
+{
+    Skills skills;
+    SMPlayerStats player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<SMPlayerStats>();
+    }
+
+    //Adds a skill onto skills
+    private void learnSkill(Skill skill) {
+        //Todo: get skills from the entity
+        skills.Learn(skill.skillName, skill.anxiety, skill.will, skill.enemyDamage);
+    } 
+}
