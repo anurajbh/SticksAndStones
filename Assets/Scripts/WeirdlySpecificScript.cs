c51e4458fad48d2e97c93381c739945f6a66423a
anurajbh <anurajbh@users.noreply.github.com>
2020-07-20 15:55:20 -0400

    Linked Credits to main menu

---
+++ Assets/Scripts/WeirdlySpecificScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WeirdlySpecificScript : MonoBehaviour
+{
+    public int buildIndex;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Cancel")!=0)
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+    }
+}
