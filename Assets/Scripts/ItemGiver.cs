738dad317b2f7ec4c88198d00b0e172c88ffe721
Amrithesh Paravath <aparavath3@gatech.edu>
2020-07-19 23:07:21 -0300

    Real items added, interactables for items WIP

---
+++ Assets/Scripts/ItemGiver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGiver : Interactable
+{
+    public DialogueOptions itemPrompt;
+    public override void Interact() {
+        if (TimeProgression.Instance.myCycle == TimeProgression.Cycle.dusk) {
+            DialogueManager.instance.AddDialogue(itemPrompt);
+        }
+    }
+
+}
